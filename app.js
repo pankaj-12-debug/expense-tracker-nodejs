cebb083193d922b68e73543cdb32d8c3822562b5
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-12-16 16:00:12 +0530

    some changes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const path=require('path');
+const fs=require('fs');
 const express=require('express');
 const bodyParser=require('body-parser');
 const bcrypt=require('bcrypt');
 const cors=require('cors');
 const sequelize=require('./util/database')
 const app=express();
-//const dotenv=require('dotenv');
+const helmet=require('helmet');
+const morgan=require('morgan');
+const dotenv=require('dotenv');
+dotenv.config();
 //routes
 const SigupRouter=require('./route/signup');
 const ExpenseRouter=require('./route/expense');
@@ -17,17 +21,20 @@ let Expenses=require('./model/expense');
 let Order=require('./model/order');
 let Forgotpassword=require('./model/password');
 let Report=require('./model/report');
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
 
 //dotenv.config();
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
+app.use(helmet());
+app.use(morgan('combined', {stream: accessLogStream}));
 app.use(SigupRouter);
 app.use(ExpenseRouter);
 app.use(purchasRouter);
 app.use(ForgotpasswordRouter);
-const dotenv=require('dotenv');
-dotenv.config();
+//const dotenv=require('dotenv');
+//dotenv.config();
 //association
 Expenses.belongsTo(User);
 User.hasMany(Expenses);
